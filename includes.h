ee050b5c58aefdb8ba1c44d551e0bfcc287b81fd
Rifat Katranci <rifatkatrani19@gmail.com>
2024-03-13 15:18:48 -0400

    test

--- includes.h
+++ includes.h
@@ -5,8 +5,8 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-#pragma comment(lib, "d3d9.lib");
-#pragma comment(lib, "d3dx9.lib");
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
 
 #include "hook.h"
 #include "drawing.h"
